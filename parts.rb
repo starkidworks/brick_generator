d53a4d10d06f30bd8eb5e8face8afe8af3531a23
starkidworks <53292696+starkidworks@users.noreply.github.com>
2019-07-25 21:53:15 +0100

    Update parts.rb

--- parts.rb
+++ parts.rb
@@ -1,12 +1,7 @@
 module MadLibs
   PARTS_OF_SPEECH = {
-    nouns:        %w(competitive\ advantage learning\ organization
-                   big\ data growth value digital\ business
-                   platform revolution culture ecosystem internet\ of\ things 
-                   insight\ from\ data virtual\ reality artificial\ intelligence blockchain design\ thinking digital\ transformation),
-    plural_nouns: %w(social\ media leaders data\ leaders),
-    adjectives:   %w(digital\ first networked collaborative cloud\ based secure 
-                   customer\ focused disruptive sustainable agile open 
-                   innovative)
+    nouns:        %w(boris),
+    plural_nouns: %w(johnson),
+    adjectives:   %w(idiot)
   }
 end
